8bc84171051b4dbb4f763efcebce30d2625f5473
danijuarez-djg <danijuarezg@icloud.com>
2026-08-31 22:16:41 -0400

    New code added

--- R_code.R
+++ R_code.R
@@ -1,2 +1,15 @@
-##Hola mundo
+### LIBRARIES
 library(tidyverse)
+library(caret)
+library(ROCR)
+library(ranger) #we'll use for our RF model
+library(mlbench) #some good built-in data sets
+
+### DATA 
+# Sonar data
+data(Sonar)
+str(Sonar) # 60 variables (sensors), and 1 classification factor 
+
+# Class - M (metal) or R (rock) 
+levels(Sonar$Class)
+Sonar %>% count(Class) %>% mutate(prop = n / sum(n)) 
